7e0a9a00668e79fc7df4a0a0e90884e62f9ac0ad
LA <la.f.bermejo@gmail.com>
2017-11-11 18:37:38 +0800

    menu and icons for admin and organization for timekeeping

--- Enterprise/Enterprise.Web/Modules/Administration/AdministrationNavigation.cs
+++ Enterprise/Enterprise.Web/Modules/Administration/AdministrationNavigation.cs
@@ -1,7 +1,7 @@
 ﻿using Serenity.Navigation;
 using Administration = Enterprise.Administration.Pages;
 
-[assembly: NavigationMenu(1000, "Administration", icon: "icon-screen-desktop")]
+[assembly: NavigationMenu(1000, "Administration", icon: "fa-gear")]
 [assembly: NavigationLink(1000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
 [assembly: NavigationLink(1000, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
 //[assembly: NavigationLink(1000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
--- Enterprise/Enterprise.Web/Modules/Organization/OrganizationNavigation.cs
+++ Enterprise/Enterprise.Web/Modules/Organization/OrganizationNavigation.cs
@@ -4,18 +4,20 @@ using Organization = Enterprise.Organization.Pages;
 
 [assembly: NavigationMenu(8000, "Organization", icon: "fa-sitemap")]
 [assembly: NavigationLink(8000, "Organization/Business Units", typeof(Organization.BusinessUnitController), icon: "fa-sitemap")]
-[assembly: NavigationLink(8000, "Organization/Personnel", typeof(MyPages.PersonnelController), icon: null)]
+[assembly: NavigationLink(8000, "Organization/Personnel", typeof(MyPages.PersonnelController), icon: "fa-users")]
 //[assembly: NavigationLink(8000, "Organization/Contacts", typeof(Organization.ContactController), icon: "fa-address-book")]
 //[assembly: NavigationLink(8000, "Organization/Meetings", typeof(Organization.MeetingController))]
 
 
-[assembly: NavigationMenu(8000, "Organization/Libraries", icon: "fa-list")]
+[assembly: NavigationMenu(8200, "Libraries", icon: "fa-list")]
 //[assembly: NavigationLink(8000, "Libraries/Personnel Status", typeof(MyPages.PersonnelStatusController), icon: null)]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Agenda Types", typeof(Organization.MeetingAgendaTypeController))]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Locations", typeof(Organization.MeetingLocationController))]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Types", typeof(Organization.MeetingTypeController))]
-[assembly: NavigationLink(8000, "Organization/Libraries/Calendar Holiday", typeof(MyPages.CalendarHolidayController), icon: null)]
-[assembly: NavigationLink(8000, "Organization/Libraries/Time Record Type", typeof(MyPages.TimeRecordTypeController), icon: null)]
+[assembly: NavigationLink(8200, "Libraries/Calendar Holiday", typeof(MyPages.CalendarHolidayController), icon: null)]
+[assembly: NavigationLink(8200, "Libraries/Time Record Type", typeof(MyPages.TimeRecordTypeController), icon: null)]
 
-[assembly: NavigationLink(8000, "TimeKeeping/Time Record", typeof(MyPages.TimeRecordController), icon: null)]
-[assembly: NavigationLink(8000, "TimeKeeping/Time Record Approval", typeof(MyPages.TimeRecordApprovalController), icon: null)]
+
+[assembly: NavigationMenu(8300, "Timekeeping", icon: "fa-clock-o")]
+[assembly: NavigationLink(8300, "Timekeeping/Time Record", typeof(MyPages.TimeRecordController), icon: null)]
+[assembly: NavigationLink(8300, "Timekeeping/Time Record Approval", typeof(MyPages.TimeRecordApprovalController), icon: null)]
