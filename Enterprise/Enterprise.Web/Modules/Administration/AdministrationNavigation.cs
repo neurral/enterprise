269ce605a289cb866ab211c9a9a22f28fd83e19b
LA <la.f.bermejo@gmail.com>
2017-11-11 20:15:30 +0800

    rename user management to users

--- Enterprise/Enterprise.Web/Modules/Administration/AdministrationNavigation.cs
+++ Enterprise/Enterprise.Web/Modules/Administration/AdministrationNavigation.cs
@@ -3,7 +3,7 @@ using Administration = Enterprise.Administration.Pages;
 
 [assembly: NavigationMenu(1000, "Administration", icon: "fa-gear")]
 [assembly: NavigationLink(1000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
-[assembly: NavigationLink(1000, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
+[assembly: NavigationLink(1000, "Administration/Users", typeof(Administration.UserController), icon: "icon-people")]
 //[assembly: NavigationLink(1000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
 //[assembly: NavigationLink(1000, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
 [assembly: NavigationMenu(1000, "Administration/Security", icon: "fa-shield")]
