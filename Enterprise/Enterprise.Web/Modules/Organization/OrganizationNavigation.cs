29c4ac785edc6d4d77239d016595bd95f31073c8
LA <la.f.bermejo@gmail.com>
2017-11-11 18:37:38 +0800

    nav and adjust nav (libraries last)

--- Enterprise/Enterprise.Web/Modules/Organization/OrganizationNavigation.cs
+++ Enterprise/Enterprise.Web/Modules/Organization/OrganizationNavigation.cs
@@ -8,16 +8,14 @@ using Organization = Enterprise.Organization.Pages;
 //[assembly: NavigationLink(8000, "Organization/Contacts", typeof(Organization.ContactController), icon: "fa-address-book")]
 //[assembly: NavigationLink(8000, "Organization/Meetings", typeof(Organization.MeetingController))]
 
+[assembly: NavigationMenu(8300, "Timekeeping", icon: "fa-clock-o")]
+[assembly: NavigationLink(8300, "Timekeeping/Time Record", typeof(MyPages.TimeRecordController), icon: null)]
+[assembly: NavigationLink(8300, "Timekeeping/Time Record Approval", typeof(MyPages.TimeRecordApprovalController), icon: null)]
 
 [assembly: NavigationMenu(8200, "Libraries", icon: "fa-list")]
 //[assembly: NavigationLink(8000, "Libraries/Personnel Status", typeof(MyPages.PersonnelStatusController), icon: null)]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Agenda Types", typeof(Organization.MeetingAgendaTypeController))]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Locations", typeof(Organization.MeetingLocationController))]
 //[assembly: NavigationLink(8000, "Organization/Libraries/Meeting Types", typeof(Organization.MeetingTypeController))]
-[assembly: NavigationLink(8200, "Libraries/Calendar Holiday", typeof(MyPages.CalendarHolidayController), icon: null)]
-[assembly: NavigationLink(8200, "Libraries/Time Record Type", typeof(MyPages.TimeRecordTypeController), icon: null)]
-
-
-[assembly: NavigationMenu(8300, "Timekeeping", icon: "fa-clock-o")]
-[assembly: NavigationLink(8300, "Timekeeping/Time Record", typeof(MyPages.TimeRecordController), icon: null)]
-[assembly: NavigationLink(8300, "Timekeeping/Time Record Approval", typeof(MyPages.TimeRecordApprovalController), icon: null)]
+[assembly: NavigationLink(8900, "Libraries/Calendar Holiday", typeof(MyPages.CalendarHolidayController), icon: null)]
+[assembly: NavigationLink(8900, "Libraries/Time Record Type", typeof(MyPages.TimeRecordTypeController), icon: null)]
