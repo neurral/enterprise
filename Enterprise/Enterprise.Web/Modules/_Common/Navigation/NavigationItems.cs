155abd25dc1dee6628d3da69b97c059b1f52847f
LA <la.f.bermejo@gmail.com>
2017-11-11 18:37:38 +0800

    icon for dashboard

--- Enterprise/Enterprise.Web/Modules/_Common/Navigation/NavigationItems.cs
+++ Enterprise/Enterprise.Web/Modules/_Common/Navigation/NavigationItems.cs
@@ -1,4 +1,4 @@
 ﻿using Serenity.Navigation;
 using Administration = Enterprise.Administration.Pages;
 
-[assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "icon-speedometer")]
+[assembly: NavigationLink(0001, "Dashboard", url: "~/", permission: "", icon: "fa-th")]
