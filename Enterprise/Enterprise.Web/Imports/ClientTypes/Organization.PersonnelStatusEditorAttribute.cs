17fcf4e08ac424ba3393a948f46670cf879683e7
LA <la.f.bermejo@gmail.com>
2017-11-12 12:25:51 +0800

    csproj atypings

---
+++ Enterprise/Enterprise.Web/Imports/ClientTypes/Organization.PersonnelStatusEditorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Serenity;
+using Serenity.ComponentModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Enterprise.Organization
+{
+    public partial class PersonnelStatusEditorAttribute : LookupEditorBaseAttribute
+    {
+        public const string Key = "Enterprise.Organization.PersonnelStatusEditor";
+
+        public PersonnelStatusEditorAttribute()
+            : base(Key)
+        {
+        }
+    }
+}
+
